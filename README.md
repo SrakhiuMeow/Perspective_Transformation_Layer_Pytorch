42834c33efdcc54f9414d4bca5e7ae49423b190f
zzs <2362169530@qq.com>
2024-03-22 16:32:06 +0800

    pers_layer.py TF->Pytorch

--- README.md
+++ README.md
@@ -1,47 +1,11 @@
-# Perspective_Transformation_Layer
-Code and experiments for perspective transformation Layer
+# Perspective_Transformation_Layer_PyTorch
+Perspective Transformation Layer ported to Pytorch
 
 
 ## Introduction
-The perspective transformation transformation layer can learn adjustable number of multiple viewpoints(homography).
+The perspective transformation transformation layer can learn adjustable number of multiple viewpoints(homography). And now it's ported to Pytorch.
+See the original [repo](https://github.com/kcnishan/Perspective_Transformation_Layer) for details.
 
-![multi-view](https://user-images.githubusercontent.com/16822926/203680319-046e1141-51f0-4a7e-98e8-ae2f8f34df95.png)
-
-
-
-## How to use perspective transformation layer
-
-```
-from pers_layer impoort *
-from utils import insert_layer
-
-updated_model = insert_layer(0,4,my_model=model)
-```
-Note: Here insert layer will insert PT layer(s) at the specified position. 
-In the above code, the first argument is the position to insert PT layer in the model; second argument is the number of transformation matrix and third 
-argument is the model we want to insert PT layer.
-
-## Some additional Application(apart from paper)
-Analogous to the state-of-the-art methods, the proposed PT layer has potential in applications of spatial attention and weak supervision. The below figure shows one of many examples in the datasets. We explain the potential using a PTL-1’s outputs of an example input image with two digits. When recognizing each digit on this two-digit image, the PT layer can learn to concentrate on the correct target digit with a viewpoint. This concentration outcome can facilitate spatial attention where we need to learn to highlight certain parts of images to help downstream tasks. Furthermore, the PT layer has the potential to assist weak supervision with inexact labels: when only providing classification labels to a deep learning model, the PT layer could also learn to centralize and localize the to-be-classified object, thus learning the object location on the images with only classification labels.
-
-
-![attention (2)](https://user-images.githubusercontent.com/16822926/203695781-ee813747-8e24-48a5-b919-41b9b28c5c42.png)
-Figure: An example visual attention. (a) The input image containing two digits;(b) and (c) The outputs of a PTL-1 when classifying it as ”3” and ”6” respectively.
-
-## Segmentation
-Image segmentation is a domain of computer vision in which a image is divided into segments like objects or part of objects comprising sets of pixels.A semantic segmentation classifies all the pixels of image into classes of objects. Apart from above described experiments, we explored the advantage of our layer for semantic segmentation task.
-
-Datasets:We used Oxford-IIIT pet datasets. The datasets consists of 37 category pet dataset consisting of 200 images for each class.
-
-Architecture:We used the famous U-net architecture  for our task. The U-net is U shaped architecture and has two major parts:contracting part(downsampling) having general convolution process and expansive part(upsampling) with transposed 2D convolution layers. We inserted two PT layers with single transformation matrix: One PT layer after the first double convolution of contracting part and the next PT layer after the last double convolution of expansive part. The size of input images were *128 by 128 by 3*.
-
-Experiment Scheme:We trained two models: the baseline U-net model and the model with PT layers under the same training configuration. We trained the model for 200 epochs and evaluated both models using dice coefficient.
-
-Results:The average dice coefficient for two models were found to be 0.75 and 0.76 respectively. The model with PT layer has better performance and better visualization results as shown in Figure below.
-
-<img width="566" alt="seg" src="https://user-images.githubusercontent.com/16822926/203696367-42452bc9-e622-4e8c-af87-5fcbdbedd0c4.png">
-
-Figure : Visualizations of segmentation results. First column: input images; Second column: target output; Third column: outputs of baseline U-net; Last column: outputs of the model with PT layer.
 
 
 
--- pers_layer.py
+++ pers_layer.py
@@ -1,29 +1,59 @@
-import tensorflow as tf
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
-from tensorflow.keras import backend as K
-from tensorflow.keras.layers import *
+import skimage
 
 
-class Perspective_Layer(tf.keras.layers.Layer): 
-    def __init__(self, output_size=None,tm=1,param=8,random_init=False, **kwargs):
-        self.output_size = output_size
-        self.param = param
-        self.tm =tm
-        self.random_init = random_init
-        super(Perspective_Layer, self).__init__(**kwargs)
-
-    def get_config(self):
-        config = super().get_config().copy()
-        config.update({
-            'output_size': self.output_size,
-            'param': self.param,
-            'tm':self.tm,
-            'random_init':self.random_init 
-        })
-        base_config = super(Perspective_Layer, self).get_config()
-        final_config = dict(list(base_config.items()) + list(config.items()))
-        return final_config
+def gather_nd(params, indices):
+    '''
+    https://discuss.pytorch.org/t/how-to-do-the-tf-gather-nd-in-pytorch/6445/26
+    4D example
+    params: tensor shaped [n_1, n_2, n_3, n_4] --> 4 dimensional
+    indices: tensor shaped [m_1, m_2, m_3, m_4, 4] --> multidimensional list of 4D indices
     
+    returns: tensor shaped [m_1, m_2, m_3, m_4]
+    
+    ND_example
+    params: tensor shaped [n_1, ..., n_p] --> d-dimensional tensor
+    indices: tensor shaped [m_1, ..., m_i, d] --> multidimensional list of d-dimensional indices
+    
+    returns: tensor shaped [m_1, ..., m_1]
+    '''
+
+    out_shape = indices.shape[:-1]
+    indices = indices.unsqueeze(0).transpose(0, -1) # roll last axis to fring
+    ndim = indices.shape[0]
+    indices = indices.long()
+    idx = torch.zeros_like(indices[0], device=indices.device).long()
+    m = 1
+    
+    for i in range(ndim)[::-1]:
+        idx += indices[i] * m 
+        m *= params.size(i)
+    out = torch.take(params, idx)
+    return out.view(out_shape)
+
+
+class PerspectiveLayer(nn.Module):
+    def __init__(self, output_size=None, input_channel=3, tm=1, param=8, random_init=False, **kwargs):
+        super(PerspectiveLayer, self).__init__()
+        self.output_size = output_size # (H, W)
+        self.param = param
+        self.tm = tm
+        self.random_init = random_init        
+
+
+        if self.random_init:
+            initial_thetas = np.random.uniform(low=-1, high=1, size=(1, 8)).repeat(self.tm, axis=0)
+        else:
+            initial_thetas = np.array([[1, 0, 0, 0, 1., 0, 0., 0.]]).repeat(self.tm, axis=0)
+
+        channel_tiles = [np.tile(initial_thetas[i],[input_channel,1]).astype('float32') for i in range(self.tm)]
+        
+        tx = np.stack(channel_tiles, axis=0)  # (tm, channel, param)
+        
+        self.wt_pers = nn.Parameter(torch.tensor(tx, dtype=torch.float32), requires_grad=True)  
 
     def compute_output_shape(self, input_shapes):
         if type(input_shapes) is list:
@@ -31,204 +61,199 @@ class Perspective_Layer(tf.keras.layers.Layer):
         else:
             input_shapes = input_shapes
         H, W = self.output_size
-        num_channels = input_shapes[-1]
-        return (None, H, W, num_channels)
-        
+        num_channels = input_shapes[1]
+        return (None, num_channels, H, W)
 
         
-    def build(self, input_shape):
-        if self.random_init==True:
-            initial_thetas = [np.random.uniform(low=-1,high=1,size=(1, 8)) for _ in range(self.tm)]
-        else:
-            initial_thetas = [np.array([[1,0,0,0,1.,0,0.,0.]]) for _ in range(self.tm)]
-            
-            
-        channel_tiles = [np.tile(initial_thetas[i],[input_shape[-1],1]).astype('float32') for i in range(self.tm)]
-       
-        tx = np.stack(channel_tiles, axis=0)
-        
-        self.wt_pers = self.add_weight(name='perspective',                                                                                                                                                            
-                                  shape = (tx.shape),                                                                                                                                      
-                                  initializer=tf.constant_initializer(tx),                                                                                                                              
-                                  trainable=True) 
-      
-        super(Perspective_Layer, self).build(input_shape) 
-    
-        
-    def call(self, inputs):
-        if type(inputs) is list:
+    def forward(self, inputs):
+        if isinstance(inputs, list):
             inp = inputs[0]
         else:
-            inp = inputs 
-            
-        expand_inp = tf.expand_dims(inp,axis=0)
-        tile_inputs = tf.tile(expand_inp,multiples=[self.tm,1,1,1,1])
-        
-        all_out = tf.vectorized_map(self.vectorize_tms,(self.wt_pers,tile_inputs))
-        all_out = tf.unstack(all_out,axis=0)
-        all_out = tf.concat(all_out, axis =-1)
+            inp = inputs
+
+        expand_inp = inp.unsqueeze(0)
+        tile_inputs = expand_inp.repeat(self.tm, 1, 1, 1, 1) # (tm, N, C, H, W)
+
+        all_out = [self.vectorize_tms((self.wt_pers[i], tile_inputs[i])) for i in range(self.tm)] # (N, C, Ho, Wo, tm)
+        all_out = torch.cat(all_out, dim=-1)
         return all_out
     
     
     def vectorize_tms(self,args):
-        wt_tms,inps = args
-        batch_size = tf.shape(inps)[0]
-        channel_first_inp = K.permute_dimensions(inps, (3,1,2,0))
-        singleTM_out =  tf.vectorized_map(self.vectorize_out,(wt_tms,channel_first_inp))
-        singleTM_out = tf.squeeze(singleTM_out, axis=-1)
-        singleTM_out = K.permute_dimensions(singleTM_out,(1,2,3,0))
+        wt_tms, inps = args # wt_tms: (C, param), inps: (N, C, H, W)
+        batch_size = inps.shape[0]
+        channels = inps.shape[1]
+        channel_first_inp = inps.permute(1, 0, 2, 3) # (C, N, H, W)
+        singleTM_out = [self.vectorize_out((wt_tms[i], channel_first_inp[i])) for i in range(channels)]
+        singleTM_out = torch.stack(singleTM_out, dim=0) # (C, N, Ho, Wo, 1)
+        # singleTM_out = singleTM_out.squeeze(-1)
+        singleTM_out = singleTM_out.permute(1, 0, 2, 3, 4) # (N, C, Ho, Wo, 1)
         return singleTM_out
 
     def vectorize_out(self,arg):
-        all_weights,inp = arg
-        inp = K.permute_dimensions(inp, (2,0,1))
-        inp2 = tf.expand_dims(inp, axis=-1)
-        batch_size = tf.shape(inp2)[0]
-        w_expand = tf.expand_dims(all_weights, axis=0)
-        wt_tile = tf.tile(w_expand, multiples=[batch_size, 1])
-        out = self.apply_transformation(inp2,wt_tile,self.output_size)
+        all_weights, inp = arg # all_weights: (param), inp: (N, H, W)
+        # inp = inp.permute(2, 0, 1)
+        inp2 = inp.unsqueeze(-1) # (N, H, W, 1)
+        batch_size = inp2.shape[0] 
+        w_expand = all_weights.unsqueeze(0) # (1, param)
+        wt_tile = w_expand.repeat(batch_size, 1) # (N, param)
+        out = self.apply_transformation(inp2, wt_tile, self.output_size) # (N, Ho, Wo, 1)
         return out
 
     def get_theta_matrix(self,theta):
-        N = tf.shape(theta)[0]
-        params_theta = tf.shape(theta)[1]
-        identity_matrix = tf.eye(3)    
-        identity_params = tf.reshape(identity_matrix, [3*3])
-        remaining_params =  identity_params[params_theta:]
-        batch_tile_remaining = tf.tile(remaining_params, [N])
+        N = theta.shape[0]
+        params_theta = theta.shape[1]
+        identity_matrix = torch.eye(3)
+        identity_params = identity_matrix.view(-1)
+        remaining_params = identity_params[params_theta:]
+        batch_tile_remaining = remaining_params.repeat(N)
         batch_params_remaining = [N, 9 - params_theta]
-        batch_remaining = tf.reshape(batch_tile_remaining,batch_params_remaining )
-        theta_final = tf.concat([theta, batch_remaining], axis=1)
+        batch_remaining = batch_tile_remaining.view(batch_params_remaining)
+        theta_final = torch.cat([theta, batch_remaining], dim=1) # (N, 9)
         return theta_final
     
     
     def apply_transformation(self,features_inp, theta, out_shape=None, **kwargs):
-
-        # get shapes of input features
-        N = tf.shape(features_inp)[0]
-        H = tf.shape(features_inp)[1]
-        W = tf.shape(features_inp)[2]
+        # features_inp (N, H, W, 1)
+        # theta (N, 9)
         
+        # get shapes of input features
+        N = features_inp.shape[0]
+        H = features_inp.shape[1]
+        W = features_inp.shape[2]
+
         # get perspective transformation parameters
         pers_matrix = self.get_theta_matrix(theta)
-        pers_matrix_shape = [N,3,3]
-        pers_matrix = tf.reshape(pers_matrix, pers_matrix_shape)
+        pers_matrix_shape = [N, 3, 3]
+        pers_matrix = pers_matrix.view(pers_matrix_shape)
 
         # Grid generation
         if out_shape:
             Ho = out_shape[0]
             Wo = out_shape[1]
-            x_s, y_s = self.generate_grids(Ho, Wo, pers_matrix)
+            x_s, y_s = self.generate_grids(Ho, Wo, pers_matrix) # (N, Ho, Wo)
         else:
-            x_s, y_s = self.generate_grids(H, W, pers_matrix)
+            x_s, y_s = self.generate_grids(H, W, pers_matrix) # (N, H, W)
 
-        features_out = self.interpolate(features_inp, x_s, y_s)
+        features_out = self.interpolate(features_inp, x_s, y_s) # (N, Ho, Wo, 1)
         return features_out
 
 
 
     def extract_pixels(self,feature, x_vect, y_vect):
+        # feature (N, H, W, 1)
+        # x,y (N, H, W)
+        N = x_vect.shape[0]
+        H = x_vect.shape[1]
+        W = x_vect.shape[2]
 
-        N = tf.shape(x_vect)[0]
-        H = tf.shape(x_vect)[1]
-        W = tf.shape(x_vect)[2]
+        batch_idx = torch.arange(0, N).view(N, 1, 1)
+        b_tile = batch_idx.repeat(1, H, W) # (N, H, W)
 
-        batch_idx = tf.range(0, N)
-        batch_idx = tf.reshape(batch_idx, (N, 1, 1))
-        b_tile = tf.tile(batch_idx, (1, H, W))
+        ind = torch.stack([b_tile, y_vect, x_vect], dim=3) # (N, H, W, 3)
 
-        ind = tf.stack([b_tile, y_vect, x_vect], 3)
-
-        pixels_value_out = tf.gather_nd(feature, ind)
+        pixels_value_out = gather_nd(feature, ind) # (N, H, W)
         return pixels_value_out
 
 
     def generate_grids(self,H, W, theta):
         #get the batch size
-        N = tf.shape(theta)[0] 
+        N = theta.size(0)
 
         # Meshgrid
-        x = tf.linspace(-1.0, 1.0, W)
-        y = tf.linspace(-1.0, 1.0, H)
-        x_g, y_g = tf.meshgrid(x, y)
+        x = torch.linspace(-1.0, 1.0, W)
+        y = torch.linspace(-1.0, 1.0, H)
+        x_g, y_g = torch.meshgrid(x, y, indexing='ij')
 
         # Flatten the meshgrid
-        flatten_x_g = tf.reshape(x_g, [-1])
-        flatten_y_g = tf.reshape(y_g, [-1])
+        flatten_x_g = x_g.flatten()
+        flatten_y_g = y_g.flatten()
 
         # reshape to [x_g, y_g , 1] - (homogeneous form)
-        ones = tf.ones_like(flatten_x_g)
-        get_grid = tf.stack([flatten_x_g, flatten_y_g, ones])
+        ones = torch.ones_like(flatten_x_g)
+        get_grid = torch.stack([flatten_x_g, flatten_y_g, ones])
 
         # get grid for each features in N
-        get_grid = tf.expand_dims(get_grid, axis=0)
-        get_grid = tf.tile(get_grid, tf.stack([N, 1, 1]))
+        get_grid = get_grid.unsqueeze(0)
+        get_grid = get_grid.repeat(N, 1, 1)
 
-        theta = tf.cast(theta, 'float32')
-        get_grid = tf.cast(get_grid, 'float32')
+        theta = theta.float()
+        get_grid = get_grid.float()
 
         # use matmul to transform the sampling grid
-        batch_grids = tf.matmul(theta, get_grid)
+        batch_grids = torch.matmul(theta, get_grid)
 
         #reshape
-        batch_grids = tf.reshape(batch_grids, [N, 3, H, W])
+        batch_grids = batch_grids.view(N, 3, H, W)
 
         #  homogenous coordinates to Cartesian
         omega = batch_grids[:, 2, :, :]
-        x_s = batch_grids[:, 0, :, :] / omega
-        y_s = batch_grids[:, 1, :, :] / omega
+        x_s = batch_grids[:, 0, :, :] / omega # (N, H, W)
+        y_s = batch_grids[:, 1, :, :] / omega # (N, H, W)
 
         return x_s, y_s
 
     def interpolate(self,img, x, y):
-        H = tf.shape(img)[1]
-        W = tf.shape(img)[2]
-        max_y = tf.cast(H - 1, 'int32')
-        max_x = tf.cast(W - 1, 'int32')
-        zero = tf.zeros([], dtype='int32')
+        # img (N, H, W, 1)
+        # x, y (N, Ho, Wo)
+        H = img.shape[1]
+        W = img.shape[2]
+        max_y = float(H - 1)
+        max_x = float(W - 1)
+        zero = torch.zeros([], dtype=torch.int32)
 
         # rescale x and y to [0, W-1/H-1]
-        x = tf.cast(x, 'float32')
-        y = tf.cast(y, 'float32')
-        x = 0.5 * ((x + 1.0) * tf.cast(max_x-1, 'float32'))
-        y = 0.5 * ((y + 1.0) * tf.cast(max_y-1, 'float32'))
+        x = x.float()
+        y = y.float()
+        x = 0.5 * ((x + 1.0) * (max_x - 1))
+        y = 0.5 * ((y + 1.0) * (max_y - 1))
 
         # do sampling
-        x0 = tf.cast(tf.floor(x), 'int32')
+        x0 = x.floor().int()
         x1 = x0 + 1
-        y0 = tf.cast(tf.floor(y), 'int32')
+        y0 = y.floor().int()
         y1 = y0 + 1
 
         # clip
-        x0 = tf.clip_by_value(x0, zero, max_x)
-        x1 = tf.clip_by_value(x1, zero, max_x)
-        y0 = tf.clip_by_value(y0, zero, max_y)
-        y1 = tf.clip_by_value(y1, zero, max_y)
+        x0 = torch.clamp(x0, zero, max_x)
+        x1 = torch.clamp(x1, zero, max_x)
+        y0 = torch.clamp(y0, zero, max_y)
+        y1 = torch.clamp(y1, zero, max_y)
 
         # extract pixel value
-        Ia = self.extract_pixels(img, x0, y0)
-        Ib = self.extract_pixels(img, x0, y1)
-        Ic = self.extract_pixels(img, x1, y0)
-        Id = self.extract_pixels(img, x1, y1)
+        Ia = self.extract_pixels(img, x0, y0).unsqueeze(3)
+        Ib = self.extract_pixels(img, x0, y1).unsqueeze(3)
+        Ic = self.extract_pixels(img, x1, y0).unsqueeze(3)
+        Id = self.extract_pixels(img, x1, y1).unsqueeze(3)
 
 
         # Finally calculate interpolated values
         # recast as float
-        x0_f = tf.cast(x0, 'float32')
-        x1_f = tf.cast(x1, 'float32')
-        y0_f = tf.cast(y0, 'float32')
-        y1_f = tf.cast(y1, 'float32')
+        x0_f = x0.float()
+        x1_f = x1.float()
+        y0_f = y0.float()
+        y1_f = y1.float()
 
-        a = (x1_f-x) * (y1_f-y)
-        b = (x1_f-x) * (y1_f-y0)
-        c = (x-x0_f) * (y1_f-y)
-        d = (x-x0_f) * (y-y0_f)
-        wa = tf.expand_dims(a, axis=3)
-        wb = tf.expand_dims(b, axis=3)
-        wc = tf.expand_dims(c, axis=3)
-        wd = tf.expand_dims(d, axis=3)
+        a = (x1_f - x) * (y1_f - y)
+        b = (x1_f - x) * (y1_f - y0)
+        c = (x - x0_f) * (y1_f - y)
+        d = (x - x0_f) * (y - y0_f)
+        wa = a.unsqueeze(3)
+        wb = b.unsqueeze(3)
+        wc = c.unsqueeze(3)
+        wd = d.unsqueeze(3)
 
         # compute output
-        output = tf.add_n([wa*Ia, wb*Ib, wc*Ic, wd*Id])
+        output = wa * Ia + wb * Ib + wc * Ic + wd * Id # (N, Ho, Wo, 1)
 
         return output
+    
+if __name__ == '__main__':
+    # Test
+    input = torch.randn(8, 3, 448, 224)
+    pers_layer = PerspectiveLayer(output_size=(448, 224), tm=4, random_init=False)
+    output = pers_layer(input)
+    print(output.shape)
+    # print(pers_layer.state_dict())
+    # print(pers_layer.wt_pers.shape)
+    # Output: torch.Size([1, 224, 224, 3])
